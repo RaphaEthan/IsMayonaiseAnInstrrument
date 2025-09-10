b02771a7f1eaffa6a269febf91710b8a27782242
RaphaM <raphael_manurung_34rpl@student.smktelkom-mlg.sch.id>
2025-09-10 09:05:24 +0700

    Add files via upload

--- Inst.java
+++ Inst.java
@@ -3,13 +3,22 @@ private String name;
 private int price;
 
 
-public Inst(){
-name = "Piano";
-price = 15000000;
+public Inst(String n, int p){
+name = n;
+price = p;
 }
+
+public String getName(){
+    return name;
+
+public int getPrice(){
+    return price;
+}
+
 public void display(){
-    System.out.println("Nama: "+name);
-    System.out.println("Harga: Rp."+price);
+    System.out.println("Nama: "+ name);
+    System.out.println("Harga: Rp."+ price);
 }
     
+}
 }
--- InstT.java
+++ InstT.java
@@ -1,8 +1,9 @@
 public class InstT {
 public static void main(String[] args) {
     
-Inst Piano = new Inst(); 
+Inst Piano = new Inst("Piano",15000000);
 Piano.display();
-
+Inst Guitar = new Inst("Guitar",1000000);
+Guitar.display();
 }
 }
